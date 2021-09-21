817023a4e167ba5ae4a26f6d9fd9f249607c0574
Mike Vickers <mvickers@gcfb.org>
2021-09-21 13:10:04 -0400

    added a deployment settings file

--- surveys/settings.py
+++ surveys/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import json
+
+secrets = json.load('deployment_settings.json')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s842k6oj6flt^53uy@50=df#u$6whtlkl^4#u^1y(jd-%pm++e'
+SECRET_KEY = secrets['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = secrets["DEBUG"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = secrets["ALLOWED_HOSTS"]
 
 
 # Application definition
